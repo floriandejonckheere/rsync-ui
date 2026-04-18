1d5518301aeb6a6c018dd96012b65cafbf0a5712
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-04-18 20:52:14 +0200

    Drop tag from status

--- app/helpers/job_run_helper.rb
+++ app/helpers/job_run_helper.rb
@@ -2,11 +2,11 @@
 
 module JobRunHelper
   JOB_RUN_STATUS_CLASSES = {
-    "pending" => "bg-gray-50 dark:bg-gray-700 text-gray-700 dark:text-gray-300",
-    "running" => "bg-blue-50 dark:bg-blue-900/30 text-blue-700 dark:text-blue-300",
-    "completed" => "bg-green-50 dark:bg-green-900/30 text-green-700 dark:text-green-300",
-    "failed" => "bg-red-50 dark:bg-red-900/30 text-red-700 dark:text-red-300",
-    "canceled" => "bg-gray-50 dark:bg-gray-700 text-gray-700 dark:text-gray-300",
+    "pending" => "text-gray-700 dark:text-gray-300",
+    "running" => "text-blue-700 dark:text-blue-300",
+    "completed" => "text-green-700 dark:text-green-300",
+    "failed" => "text-red-700 dark:text-red-300",
+    "canceled" => "text-gray-700 dark:text-gray-300",
   }.freeze
 
   def job_run_status_classes(status)
