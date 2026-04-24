82ff37e86dfa74589f3cf7d9dfe58589da8e717b
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-04-24 21:55:37 +0200

    Update pagination

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -2,6 +2,6 @@
 
 module PaginationHelper
   def pagination_page_path(path_method, page_number, **params)
-    send(path_method, page: page_number, **params)
+    send(path_method, **params, page: page_number)
   end
 end
