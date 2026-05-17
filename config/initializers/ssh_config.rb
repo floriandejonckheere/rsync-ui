895a0172e03ce3b42b7fc5d54051ded91c624682
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-05-17 10:47:32 +0200

    Skip syncing SSH config file if database doesn't exist

--- config/initializers/ssh_config.rb
+++ config/initializers/ssh_config.rb
@@ -2,6 +2,9 @@
 
 Rails.application.config.after_initialize do
   next if Rails.env.test?
+  next unless ActiveRecord::Base.connection.table_exists?(:servers)
 
   Servers::SyncSSHConfigJob.perform_later
+rescue ActiveRecord::NoDatabaseError, ActiveRecord::StatementInvalid
+  nil
 end
