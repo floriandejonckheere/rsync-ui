c67ce7aa159bbb714232061cc1db200c3d1cd575
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-05-17 10:52:26 +0200

    Run dev seeds on startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 bundle exec rails db:prepare
 bundle exec rails database:seed:production
 
+if [ "${RAILS_ENV}" = "development" ]; then
+  bundle exec rails database:seed:development
+fi
+
 exec "$@"
