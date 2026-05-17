115ab8fbd529dcf276d8369d2a14f44fa605088a
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-05-17 14:19:00 +0200

    Render server on repository if applicable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,37 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def server_link(server, absolute_url: false)
+    return if server.blank?
+
+    url  = absolute_url ? edit_server_url(server) : edit_server_path(server)
+    data = {
+      turbo_frame: "_top",
+      tooltip: server.description.presence,
+    }.compact_blank
+
+    link_to(
+      server.name,
+      url,
+      class: "hover:underline",
+      title: server.description.presence,
+      data:,
+    )
+  end
+
+  def on_server_tag(server, absolute_url: false)
+    return if server.blank?
+
+    tag.span(class: "text-xs text-gray-400 dark:text-gray-500") do
+      t("jobs.table.on_server_html", server_html: server_link(server, absolute_url:))
+    end
+  end
+
+  def on_server_label(server)
+    return if server.blank?
+
+    tag.span(class: "text-xs text-gray-400 dark:text-gray-500") do
+      " #{I18n.t('jobs.table.on_server', server: server.name)}"
+    end
+  end
 end
