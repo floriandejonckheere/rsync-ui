d849e7fa596fd61f3f64985349eb640605c4c7ee
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-05-19 19:47:17 +0200

    Allow skipping syncing SSH config file on startup

--- config/initializers/ssh_config.rb
+++ config/initializers/ssh_config.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+return if ENV.fetch("SKIP_SSH_CONFIG_SYNC", "0") == "1"
+
 Rails.application.config.after_initialize do
   next if Rails.env.test?
   next unless ActiveRecord::Base.connection.table_exists?(:servers)
