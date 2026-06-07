e7773c9c286392010fe0cf485f7a71b64a56d9ae
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-06-07 10:44:31 +0200

    Disable audits by default

--- spec/services/servers/connection_service_spec.rb
+++ spec/services/servers/connection_service_spec.rb
@@ -3,7 +3,9 @@
 RSpec.describe Servers::ConnectionService do
   subject(:service) { described_class.new(server) }
 
-  let(:server) { build(:server, :with_password) }
+  with_configuration "audits" => false
+
+  let(:server) { create(:server, :with_password) }
 
   before { stub_ssh(output: "ok\n") }
 
@@ -28,23 +30,19 @@ RSpec.describe Servers::ConnectionService do
         .with(server.host, server.username, hash_including(port: server.port, password: server.password, auth_methods: ["password"]))
     end
 
-    context "when server is persisted" do
-      let(:server) { create(:server, :with_password) }
+    it "updates probed_at and last_seen_at on success" do
+      service.call
 
-      it "updates probed_at and last_seen_at on success" do
-        service.call
+      server.reload
 
-        server.reload
-
-        expect(server.probed_at).to be_within(5.seconds).of(Time.zone.now)
-        expect(server.last_seen_at).to be_within(5.seconds).of(Time.zone.now)
-        expect(server.error_class).to be_nil
-        expect(server.error_message).to be_nil
-      end
+      expect(server.probed_at).to be_within(5.seconds).of(Time.zone.now)
+      expect(server.last_seen_at).to be_within(5.seconds).of(Time.zone.now)
+      expect(server.error_class).to be_nil
+      expect(server.error_message).to be_nil
     end
 
     context "when ssh_key is provided instead of password" do
-      let(:server) { build(:server, :with_ssh_key) }
+      let(:server) { create(:server, :with_ssh_key) }
 
       it "passes the correct SSH options" do
         service.call
@@ -66,19 +64,15 @@ RSpec.describe Servers::ConnectionService do
         expect(service.call).to eq success: false, message: "Net::SSH::AuthenticationFailed: Authentication failed for admin@example.com"
       end
 
-      context "when server is persisted" do
-        let(:server) { create(:server, :with_password) }
+      it "updates probed_at and error fields, leaves last_seen_at unchanged" do
+        service.call
 
-        it "updates probed_at and error fields, leaves last_seen_at unchanged" do
-          service.call
+        server.reload
 
-          server.reload
-
-          expect(server.probed_at).to be_within(5.seconds).of(Time.zone.now)
-          expect(server.last_seen_at).to be_nil
-          expect(server.error_class).to eq("Net::SSH::AuthenticationFailed")
-          expect(server.error_message).to include("Authentication failed")
-        end
+        expect(server.probed_at).to be_within(5.seconds).of(Time.zone.now)
+        expect(server.last_seen_at).to be_nil
+        expect(server.error_class).to eq("Net::SSH::AuthenticationFailed")
+        expect(server.error_message).to include("Authentication failed")
       end
     end
 
