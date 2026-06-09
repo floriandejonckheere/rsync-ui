5f2877d0271c316a3109e115a0cc793a4440bb5c
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-06-09 20:39:40 +0200

    Limit SSHConfigJob concurrency

--- app/jobs/servers/sync_ssh_config_job.rb
+++ app/jobs/servers/sync_ssh_config_job.rb
@@ -2,6 +2,10 @@
 
 module Servers
   class SyncSSHConfigJob < ApplicationJob
+    limits_concurrency to: 1,
+                       key: "sync_ssh_config_job",
+                       duration: 15.seconds
+
     def perform
       SSHConfigService.call
     end
