243b4ff85fce210e04f7dce214a87c533dc3396d
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-09-23 21:53:10 +0200

    Fix app helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,19 +3,19 @@
 RSpec.describe ApplicationHelper do
   describe "#page_title" do
     it "returns the application name without a page title" do
-      expect(helper.page_title).to eq "rsync ui"
+      expect(helper.page_title).to eq "Rsync UI"
     end
 
     it "prefixes the page title with the application name" do
       helper.content_for(:title) { "\n  Jobs\n" }
 
-      expect(helper.page_title).to eq "rsync ui - Jobs"
+      expect(helper.page_title).to eq "Jobs - Rsync UI"
     end
 
     it "does not double-escape the page title" do
       helper.content_for(:title, "Tom's & Jerry's")
 
-      expect(helper.page_title).to eq "rsync ui - Tom's & Jerry's"
+      expect(helper.page_title).to eq "Tom's & Jerry's - Rsync UI"
     end
   end
 end
